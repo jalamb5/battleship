caf1badbb34d99f5b727b91f508d42d32debc40f
Justin Lamb <justinlamb@me.com>
2023-10-18 11:20:09 -0700

    place ships of size > 1

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,11 +5,25 @@ export default class Gameboard {
     this.allShips = [];
   };
 
-  placeShip(size, coordinates) {
+  placeShip(size, firstCoord, orientation='horizontal') {
+    const coordinates = this.#buildCoordinates(size, firstCoord, orientation);
     const newShip = new Ship(size);
-    const obj = { newShip : coordinates };
-    this.allShips.push(obj);
+    const shipEntry = [newShip, coordinates];
+    this.allShips.push(shipEntry);
+  }
+
+  #buildCoordinates(size, firstCoord, orientation) {
+    let coordinates = [];
+    for (let i = 0; i < size; i++) {
+      if (orientation === 'horizontal') {
+        coordinates.push([firstCoord[0] + i, firstCoord[1]]);
+      } else {
+        coordinates.push([firstCoord[0], firstCoord[1] + i]);
+      }
+    }
+    return coordinates;
   }
 }
 
 module.exports = Gameboard;
+
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -5,3 +5,22 @@ test('a ship is placed at a coordinate', () => {
   board.placeShip(1, [0, 0]);
   expect(board.allShips.length).toBe(1);
 })
+
+test('multiple ships can be placed on board', () => {
+  const board = new gameboard;
+  board.placeShip(1, [0, 0]);
+  board.placeShip(2, [1, 0]);
+  expect(board.allShips.length).toBe(2);
+})
+
+test('ship of size 4 is placed at mutliple coordinates', () => {
+  const board = new gameboard;
+  board.placeShip(4, [0, 0]);
+  expect(board.allShips[0][1]).toStrictEqual([[0, 0],[1, 0],[2, 0], [3, 0]])
+})
+
+test('ship of size 20 is placed at multiple coordinates', () => {
+  const board = new gameboard;
+  board.placeShip(20, [0, 0]);
+  expect(board.allShips[0][1].length).toBe(20);
+})
