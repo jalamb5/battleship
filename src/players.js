7bf531d22cab3dcc759d1f95794b119ebe07966f
Justin Lamb <justinlamb@me.com>
2023-10-23 13:04:13 -0700

    basic ai player

--- src/players.js
+++ src/players.js
@@ -1,9 +1,35 @@
 import Gameboard from "./gameboard";
 
 export default class Player {
-  constructor() {
+  constructor(human=true) {
     this.board = new Gameboard;
+    this.isHuman = human;
+    this.previousPlays = [];
   };
+
+  attack(player, coordinate) {
+    if (!this.isHuman) {
+      coordinate = this.#aiAttack(player.board);
+    }
+    
+    this.previousPlays.push(coordinate);
+    player.board.receiveAttack(coordinate);
+  }
+
+  #aiAttack(board) {
+    let coordinate = this.#randomCoord();
+    if (this.previousPlays.includes(coordinate)) {
+      this.#aiAttack(board);
+    } else {
+
+      return coordinate;
+    }
+  }
+
+  // Generate random coordinates between 0 - 9.
+  #randomCoord() {
+    return [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+  }
 }
 
 module.exports = Player;
--- tests/players.test.js
+++ tests/players.test.js
@@ -1,6 +1,25 @@
 const Player = require('../src/players');
 
-test('a player is created', () => {
-  const player = new Player;
-  expect(player).toBeTruthy();
+test('a human player is created', () => {
+  const player = new Player(true);
+  expect(player.isHuman).toBe(true);
 });
+
+test('an ai player is created', () => {
+  const player = new Player(false);
+  expect(player.isHuman).toBe(false);
+})
+
+test('a human player can attack another player', () => {
+  const humanPlayer = new Player(true);
+  const aiPlayer = new Player(false);
+  humanPlayer.attack(aiPlayer, [0, 0]);
+  expect(aiPlayer.board.missedShots.length).toBe(1);
+})
+
+test('an ai player can attack another player', () => {
+  const humanPlayer = new Player(true);
+  const aiPlayer = new Player(false);
+  aiPlayer.attack(humanPlayer);
+  expect(humanPlayer.board.missedShots.length).toBe(1);
+})
